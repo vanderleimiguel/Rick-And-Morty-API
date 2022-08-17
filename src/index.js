3a327a4d0d3c778bf5bea4527513e5d08a6e0d59
vanderlei miguel <vanderleimiguel@hotmail.com>
2022-08-17 10:01:05 -0300

    connected data base

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const connectDatabase = require('./database/database')
 
 const port = process.env.PORT || 3002
 const app = express()
 
+connectDatabase()
 app.use(cors())
-
-app.get('/', (req, res) => {
-  res.send({ message: 'hell word!' })
-})
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`)
